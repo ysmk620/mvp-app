185b02f852b17dba6953af3e78bdcbcba2de2cd8
ysmk <0620komako@gmail.com>
2025-07-17 21:41:25 +0900

    feat: Create Cosmetic model and migration file

---
+++ database/migrations/2025_07_17_123510_create_cosmetics_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cosmetics', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('brand')->nullable();
+            $table->string('category')->nullable();
+            $table->date('expiration_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cosmetics');
+    }
+};
