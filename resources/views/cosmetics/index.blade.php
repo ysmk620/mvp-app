26263e766cf6170cc383e9961a3a656e553f73ab
ysmk <0620komako@gmail.com>
2025-07-19 10:47:31 +0900

    feat:一覧表示画面のビュー作成

---
+++ resources/views/cosmetics/index.blade.php
@@ -0,0 +1,32 @@
+<h1>コスメ一覧</h1>
+
+@if(session('success'))
+<p>{{ session('success') }}</p>
+@endif
+
+<table border="1" cellpadding="8">
+    <thead>
+        <tr>
+            <th>商品名</th>
+            <th>ブランド</th>
+            <th>カテゴリ</th>
+            <th>使用期限</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($cosmetics as $cosmetic)
+        <tr
+            @if($cosmetic->expiration_date && $cosmetic->expiration_date < \Carbon\Carbon::today()->toDateString())
+                style="color: red;"
+                @endif
+                >
+                <td>{{ $cosmetic->name }}</td>
+                <td>{{ $cosmetic->brand }}</td>
+                <td>{{ $cosmetic->category }}</td>
+                <td>{{ $cosmetic->expiration_date }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+<p><a href="{{ route('cosmetics.create') }}">新しくコスメを登録する</a></p>
