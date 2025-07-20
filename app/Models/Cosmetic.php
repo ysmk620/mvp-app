a939ee641ead5d9fe9b8d5fc38cdee7cce4e2a79
ysmk <0620komako@gmail.com>
2025-07-20 13:48:19 +0900

    fix:リレーションを定義(belongsTo を追加)+モデル側の $fillable に category_id を追加(#9)

--- app/Models/Cosmetic.php
+++ app/Models/Cosmetic.php
@@ -9,7 +9,12 @@ class Cosmetic extends Model
     protected $fillable = [
         'name',
         'brand',
-        'category',
+        'category_id',
         'expiration_date',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
