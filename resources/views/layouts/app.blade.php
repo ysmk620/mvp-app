4e688887cc6745383998456f00064c86b88ec72e
ysmk <0620komako@gmail.com>
2025-07-23 23:35:20 +0900

    chore:CDN→ビルド方式への切り替え＋CSS変数を設定(#11)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>コスメ管理アプリ(MVP)</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+   @vite('resources/css/app.css')
 </head>
 
 <body class="bg-gray-50 min-h-screen flex flex-col">
